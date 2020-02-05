c72169ff315c8e5759b3d6dcf332d469e13659a9
J David Smith <emallson@atlanis.net>
2020-02-05 11:39:39 -0500

    new test regime

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,48 @@
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck_macros;
 use num::integer::binomial;
 use num::{Float, FromPrimitive};
 
-// Estimates the first $k$ moments of a distribution in an online fashion (either each datum being
-// incrementally added, or sets of data being added in batches). There is no need to use this
-// method if you are working with complete data. Convenience accessors for several commonly-used
-// moments are provided.
-//
-// The most commonly used moment of a distribution is the *variance*, which is the second
-// standardized moment and provided by the `variance()` method.
-//
-// The update formulas from [Pébay 2008] are used. If this is used in academic work, please cite
-// this library and [Pébay 2008].
+/// Estimates the first $k$ moments of a distribution in an online fashion (either each datum being
+/// incrementally added, or sets of data being added in batches). There is no need to use this
+/// method if you are working with complete data. Convenience accessors for several commonly-used
+/// moments are provided.
+///
+/// The most commonly used moment of a distribution is the *variance*, which is the second
+/// standardized moment and provided by the `variance()` method.
+///
+/// The update formulas from [Pébay 2008] are used. If this is used in academic work, please cite
+/// this library and [Pébay 2008].
+///
+/// # Example
+///
+/// If you're only interested in the mean, you only need to calculate the first moment.
+/// ```
+/// use online_moments::MomentEstimator;
+/// let mut est = MomentEstimator::new(1);
+/// est.update(0.0);
+/// est.update(1.0);
+/// est.update(-1.0);
+/// assert_eq!(est.mean(), 0.0);
+/// assert_eq!(est.variance(), None);
+/// assert_eq!(est.skewness(), None);
+/// ```
+///
+/// Otherwise, you can supply higher moments to estimate (for example) variance and skewness. Note
+/// that higher moments require more samples to estimate accurately.
+/// ```
+/// # use online_moments::MomentEstimator;
+/// let mut est = MomentEstimator::<f64>::new(3);
+/// for _ in 0..1_000 {
+///   est.update(0.0);
+///   est.update(1.0);
+///   est.update(-1.0);
+/// }
+/// assert!(est.mean().abs() < 0.005);
+/// assert!((2.0 / 3.0 - est.variance().unwrap()).abs() < 0.005);
+/// assert!(est.skewness().unwrap().abs() < 0.005);
+/// ```
 #[derive(Debug)]
 pub struct MomentEstimator<T: FromPrimitive + Copy + Float = f64> {
     mean: T,
@@ -90,53 +122,81 @@ impl<T: FromPrimitive + Float + Copy + std::iter::Sum> MomentEstimator<T> {
 #[cfg(test)]
 mod test {
     use super::MomentEstimator;
+    use quickcheck::TestResult;
 
-    #[test]
-    fn standard_normal() {
-        use rand::{distributions::Distribution, thread_rng};
-        use statrs::distribution::Normal;
-        let dist = Normal::new(0.0, 1.0).unwrap();
+    // at most 3% error. this strikes a balance between tightness and false errors due to
+    // randomness. in the future, I should replace this with failure rate logic
+    const THRESHOLD: f64 = 0.03;
 
-        let mut est = MomentEstimator::new(3);
-
-        println!("{:?}", est);
-
-        for sample in dist.sample_iter(thread_rng()).take(1_000_000) {
-            est.update(sample);
-        }
-
-        println!("{:?}", est);
-
-        println!(
-            "{} {} {}",
-            est.mean(),
-            est.variance().unwrap(),
-            est.skewness().unwrap()
-        );
-        assert!(est.mean().abs() < 1e-2);
-        assert!((est.variance().unwrap().sqrt() - 1.0).abs() < 1e-2);
-        assert!(est.skewness().unwrap().abs() < 1e-2);
+    #[inline]
+    fn pct_err(approx: f64, exact: f64) -> f64 {
+        (approx - exact).abs() / exact
     }
 
-    #[test]
-    fn exponential() {
+    #[quickcheck]
+    fn normal(mean: f64, variance: f64) -> TestResult {
+        if variance <= 0.0 {
+            return TestResult::discard();
+        }
         use rand::{distributions::Distribution, thread_rng};
-        use statrs::distribution::Exponential;
-        let dist = Exponential::new(1.0).unwrap();
+        use statrs::distribution::Normal;
+        let dist = Normal::new(mean, variance.sqrt()).unwrap();
 
         let mut est = MomentEstimator::new(3);
-
-        println!("{:?}", est);
 
         for sample in dist.sample_iter(thread_rng()).take(1_000_000) {
             est.update(sample);
         }
 
-        println!("{:?}", est);
-        println!("{:?} {:?} {:?}", est.mean(), est.variance(), est.skewness());
+        // when the mean is approx. 0, we use additive error instead
+        let mean_err = if mean.abs() < THRESHOLD {
+            est.mean()
+        } else {
+            pct_err(est.mean(), mean)
+        };
+        let var_err = pct_err(est.variance().unwrap(), variance);
+        // skewness of the normal distribution is 0 so we can't calculate percentage error.
+        // instead, we're going to check that the absolute error is less than THRESHOLD - 1.0
+        let skew_err = est.skewness().unwrap();
 
-        assert!((est.mean() - 1.0).abs() < 1e-2);
-        assert!((est.variance().unwrap() - 1.0).abs() < 1e-2);
-        assert!((est.skewness().unwrap() - 2.0).abs() < 1e-2);
+        // println!(
+        // "{} {} {:?} {} {} {}",
+        // mean, variance, est, mean_err, var_err, skew_err
+        // );
+
+        TestResult::from_bool(
+            [mean_err, var_err, skew_err]
+                .iter()
+                .all(|&e| e <= THRESHOLD),
+        )
+    }
+
+    #[quickcheck]
+    fn exponential(lambda: f64) -> TestResult {
+        if lambda <= 0.0 {
+            return TestResult::discard();
+        }
+
+        use rand::{distributions::Distribution, thread_rng};
+        use statrs::distribution::Exponential;
+        let dist = Exponential::new(lambda).unwrap();
+
+        let mut est = MomentEstimator::new(3);
+
+        for sample in dist.sample_iter(thread_rng()).take(1_000_000) {
+            est.update(sample);
+        }
+
+        let mean_err = pct_err(est.mean(), 1.0 / lambda);
+        let var_err = pct_err(est.variance().unwrap(), 1.0 / lambda.powi(2));
+        let skew_err = pct_err(est.skewness().unwrap(), 2.0);
+
+        // println!("{} {:?} {} {} {}", lambda, est, mean_err, var_err, skew_err);
+
+        TestResult::from_bool(
+            [mean_err, var_err, skew_err]
+                .iter()
+                .all(|&e| e <= THRESHOLD),
+        )
     }
 }
